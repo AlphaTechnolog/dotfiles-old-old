9dc25cf19172590247aa192993f00174730390b6
gabriel <gfranklings@gmail.com>
2022-02-18 13:05:35 -0400

    added improvements like autoshell setup to install-dotbot version

--- dotbot-tools/respald-folders.sh
+++ dotbot-tools/respald-folders.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 declare -a folders=('.config' '.local/share' '.local/bin')
 for folder in ${folders[@]}; do
-  if ! test -d ~/$folder; then
-    mkdir -p ~/$folder
+  if ! test -d $HOME/$folder; then
+    mkdir -p $HOME/$folder
   fi
   for subfolder in $(ls ./$folder); do
-    if test -d ~/$folder/$subfolder; then
+    if test -d $HOME/$folder/$subfolder; then
       echo "Moving $HOME/$folder/$subfolder -> $HOME/$folder/$subfolder.old"
-      mv ~/$folder/$subfolder ~/$folder/$subfolder.old
+      mv $HOME/$folder/$subfolder $HOME/$folder/$subfolder.old
     fi
-    if test -f ~/$folder/$subfolder; then
+    if test -f $HOME/$folder/$subfolder; then
       echo "Moving $HOME/$folder/$subfolder -> $HOME/$folder/$subfolder.old"
-      mv ~/$folder/$subfolder ~/$folder/$subfolder.old
+      mv $HOME/$folder/$subfolder $HOME/$folder/$subfolder.old
     fi
   done
+done
+declare -a files=('.bashrc' '.zshrc' '.p10k.zsh')
+for file in ${files[@]}; do
+  if test -f $HOME/$file; then
+    echo "Moving $HOME/$file to $HOME/$file.old"
+    mv $HOME/$file $HOME/$file.old
+  fi
+  cp -r ./$file $HOME/$file
 done
