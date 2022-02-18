e5790af117a3de8fe6a3168a111146ef4499c189
gabriel <gfranklings@gmail.com>
2022-02-17 20:49:14 -0400

    some fixes

--- install-dotbot.sh
+++ install-dotbot.sh
@@ -10,5 +10,6 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "${BASEDIR}"
 git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
+git submodule update --init --recursive
 
 "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
