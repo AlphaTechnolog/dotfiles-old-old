2507082ff19478bf6fc1f24793a0ecc29fb27fe3
Gabriel <gfranklings@gmail.com>
2022-03-05 18:31:10 -0400

    replaced bspwm-dotfiles to .dotfiles in wallpapers paths

--- .config/chadwm/scripts/wallpaper.sh
+++ .config/chadwm/scripts/wallpaper.sh
@@ -5,31 +5,31 @@ main () {
 }
 
 wall_catppuccin () {
-  echo $HOME/.bspwm-dotfiles/.sample-wallpapers/catppuccin-cat.png
+  echo $HOME/.dotfiles/.sample-wallpapers/catppuccin-cat.png
 }
 
 wall_tokyonight () {
-  echo $HOME/.bspwm-dotfiles/.sample-wallpapers/tokyonight-techno.png
+  echo $HOME/.dotfiles/.sample-wallpapers/tokyonight-techno.png
 }
 
 wall_gruvbox () {
-  echo $HOME/.bspwm-dotfiles/.sample-wallpapers/gruvbox-city.png
+  echo $HOME/.dotfiles/.sample-wallpapers/gruvbox-city.png
 }
 
 wall_material_ocean () {
-  echo $HOME/.bspwm-dotfiles/.sample-wallpapers/material-ocean.jpg
+  echo $HOME/.dotfiles/.sample-wallpapers/material-ocean.jpg
 }
 
 wall_onedark () {
-  echo $HOME/.bspwm-dotfiles/.sample-wallpapers/onedark.png
+  echo $HOME/.dotfiles/.sample-wallpapers/onedark.png
 }
 
 wall_everforest () {
-  echo $HOME/.bspwm-dotfiles/.sample-wallpapers/everforest.jpg
+  echo $HOME/.dotfiles/.sample-wallpapers/everforest.jpg
 }
 
 wall_everblush () {
-  echo $HOME/.bspwm-dotfiles/.sample-wallpapers/everblush.png
+  echo $HOME/.dotfiles/.sample-wallpapers/everblush.png
 }
 
 load_wallpaper () {
