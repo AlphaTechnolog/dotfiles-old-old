14d61c982d3db4d6316ded1252ee232fd984be16
Gabriel <gfranklings@gmail.com>
2022-03-09 13:24:43 -0400

    fixed wrong paths

--- .config/bspwm/wallpapers.sh
+++ .config/bspwm/wallpapers.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 wall_catppuccin () {
-  echo $HOME/.wallpapers/.sample-wallpapers/catppuccin-bars.png
+  echo $HOME/.dotfiles/.sample-wallpapers/catppuccin-bars.png
 }
 
 wall_tokyonight () {
-  echo $HOME/.wallpapers/.sample-wallpapers/tokyonight-techno.jpg
+  echo $HOME/.dotfiles/.sample-wallpapers/tokyonight-techno.jpg
 }
 
 wall_gruvbox () {
-  echo $HOME/.wallpapers/.sample-wallpapers/gruvbox-city.png
+  echo $HOME/.dotfiles/.sample-wallpapers/gruvbox-city.png
 }
 
 wall_material_ocean () {
-  echo $HOME/.wallpapers/.sample-wallpapers/material-ocean.jpg
+  echo $HOME/.dotfiles/.sample-wallpapers/material-ocean.jpg
 }
 
 wall_onedark () {
-  echo $HOME/.wallpapers/.sample-wallpapers/onedark.png
+  echo $HOME/.dotfiles/.sample-wallpapers/onedark.png
 }
 
 wall_everforest () {
-  echo $HOME/.wallpapers/.sample-wallpapers/everforest.jpg
+  echo $HOME/.dotfiles/.sample-wallpapers/everforest.jpg
 }
 
 wall_everblush () {
-  echo $HOME/.wallpapers/.sample-wallpapers/everblush.png
+  echo $HOME/.dotfiles/.sample-wallpapers/everblush.png
 }
 
 wall_aquarium () {
-  echo $HOME/.wallpapers/.sample-wallpapers/aquarium.png
+  echo $HOME/.dotfiles/.sample-wallpapers/aquarium.png
 }
